bd7b6c6b86d154be93f6e26e4fdc9ee1d1839c18
Rionecnirp <alex.ben-larbi@hotmail.fr>
2025-06-16 14:24:11 +0200

    16/06/2025 - 14h25 : Ajout du package CORS, pour l'instant c'est non fonctionnel, je retournerai dessus plus tard

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -3,29 +3,39 @@
 const email = document.querySelector("#email").value
 const mdp = document.querySelector("#mdp").value
 const connexion =document.querySelector(".login-form")
+const cors = require('cors')
+
+app.use(cors());
+
+app.get('/data', (req, res) => {
+  res.json({ message: 'Cette réponse est CORS activé.' });
+});
+
+app.listen(5500, () => {
+  console.log('Serveur en cours d\'exécution sur le port 5500');
+});
 
 connexion.addEventListener("submit", async (event) => {
     event.preventDefault()
 
     try {
-        const envoiIdentifiant = await fetch ("http://localhost:5678/users/login", {
+        const envoiIdentifiant = await fetch ("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: {
-                "content-type" : "application/json"
+                "Content-type" : "application/json"
             },
             body: JSON.stringify({
                 email,
                 mdp
-            })
+            }),
         })
         console.log(envoiIdentifiant)
 
         const retourIdentifiant = await envoiIdentifiant.json()
 
         if (retourIdentifiant.ok) {
-            // Connexion réussie
             sessionStorage.setItem("token", result.token); 
-            window.location.href = "index.html"; 
+            window.location.href = "../index.html"; 
         } else {
             alert("Email ou mot de passe incorrect.");
         }
